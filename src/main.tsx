f4fb76a684018a126ad355cde212ee019debb687
Anuj Gajbhiye <anujgajbhiye@Anujs-MacBook-Air.local>
2026-04-22 08:20:10 +0100

    added docker file and added sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.css'
-import App from './app/App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/global.css";
+import App from "./app/App";
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById('root')!).render(
+Sentry.init({
+  dsn: "https://576656ce9f166178d09577c142fa5dbd@o4511259794997248.ingest.de.sentry.io/4511259932819536",
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0,
+  sendDefaultPii: true,
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Sentry.ErrorBoundary fallback={<p>Something went wrong</p>}>
+      <App />
+    </Sentry.ErrorBoundary>
   </StrictMode>,
-)
+);
